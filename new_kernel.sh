f091ca85c5f3c8eb36ab3111e735ec34a28a79b4
FlareXF <79987098+FlareXF@users.noreply.github.com>
2021-08-29 02:21:11 +0300

    Update new_kernel.sh

--- new_kernel.sh
+++ new_kernel.sh
@@ -2,15 +2,9 @@
 
 apt-get install build-essential linux-source bc kmod cpio flex libncurses5-dev libelf-dev libssl-dev rsync wget curl dwarves bison git
 cd /usr/src && wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.13.13.tar.xz
-tar -xavf /usr/src/linux-5.13.13.tar.xz -C /usr/src/linux-5.13.13
+tar -xvf /usr/src/linux-5.13.13.tar.xz -C /usr/src/linux-5.13.13
 cp /boot/config-* /usr/src/
-mv /usr/src/linux-5.13.13 /usr/src/.config
-
-if [ `pwd` = "/usr/src/linux-5.13.13" ]; then
-	install_ker
-else
-	cd /usr/src/linux-5.13.13 && install_ker
-fi
+mv /usr/src/linux-5.13.13/config-* /usr/src/.config
 
 install_ker () {
 	yes "" | make oldconfig
@@ -19,3 +13,8 @@ install_ker () {
 	[ -e linux-image-5.13.13_5.13.13-1_amd64.deb ] && dpkg -i linux-image-5.13.13_5.13.13-1_amd64.deb && update-grub
 }
 
+if [ `pwd` = "/usr/src/linux-5.13.13" ]; then
+	install_ker
+else
+	cd /usr/src/linux-5.13.13 && install_ker
+fi
